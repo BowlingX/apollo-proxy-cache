cf4aeab2c2277f5cb114ccf101bad73ccfeef13a
BowlingX <me@bowlingx.com>
2021-03-05 18:43:26 +0100

    feat(browser): create separate build for browser and nodejs

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,38 @@
-module.exports =  {
-    presets: [
-      '@babel/typescript',
-      [
-        '@babel/preset-env'
-      ]
+module.exports = {
+  presets: [
+    '@babel/typescript',
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: '14',
+        },
+      },
     ],
-    plugins: [
-      'lodash',
-      '@babel/proposal-class-properties'
-    ]
+  ],
+  plugins: ['lodash', '@babel/proposal-class-properties'],
+  env: {
+    browser: {
+      plugins: [
+        [
+          '@babel/plugin-transform-runtime',
+          {
+            regenerator: true,
+          },
+        ],
+      ],
+      presets: [
+        '@babel/typescript',
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              chrome: '58',
+              ie: '11',
+            },
+          },
+        ],
+      ],
+    },
+  },
 }
